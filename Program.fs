9f50392d690bd5eb16dd7278ae20ecbe9ecf15ff
MarkNovik <novikmark@gmail.com>
2023-06-25 12:41:07 +0300

    Day 2 fixed

--- Program.fs
+++ Program.fs
@@ -1,6 +1,6 @@
 ﻿open System.IO
 
-let input reader num = reader <| $"C:/Users/novik/IdeaProjects/aoc-fs/input/day{num}.txt"
+let input reader num = reader <| $"./input/day{num}.txt"
 let inputLines = input File.ReadAllLines 
 let inputText = input File.ReadAllText
 
@@ -16,70 +16,39 @@ module day1 =
     let public part2 input = input |> Array.map int |> Array.map fuelByMassFull |> Array.sum
 
 module day2 = 
-    let gravityAssist (program: int array) =
-        let mutable memory = program
-        let rec runProgram pc =
-            match memory.[pc] with
-            | 1 ->
-                let a = memory.[memory.[pc + 1]]
-                let b = memory.[memory.[pc + 2]]
-                let c = memory.[pc + 3]
-                memory.[c] <- a + b
-                runProgram (pc + 4)
-            | 2 ->
-                let a = memory.[memory.[pc + 1]]
-                let b = memory.[memory.[pc + 2]]
-                let c = memory.[pc + 3]
-                memory.[c] <- a * b
-                runProgram (pc + 4)
-            | 99 -> ()
-            | _ -> failwith "Unknown opcode"
-        memory.[1] <- 12
-        memory.[2] <- 2
-        runProgram 0
-        memory.[0]
+    let gravityAssist noun verb (program: int array) =
+            let mutable memory = Array.copy program
+            let rec runProgram pc =
+                match memory.[pc] with
+                | 1 ->
+                    memory.[memory.[pc + 3]] <- memory.[memory.[pc + 1]] + memory.[memory.[pc + 2]]
+                    runProgram (pc + 4)
+                | 2 ->
+                    memory.[memory.[pc + 3]] <- memory.[memory.[pc + 1]] * memory.[memory.[pc + 2]]
+                    runProgram (pc + 4)
+                | 99 -> ()
+                | _ -> failwith $"Unknown opcode {memory.[pc]}"
+            memory.[1] <- noun
+            memory.[2] <- verb
+            runProgram 0
+            memory.[0]
 
-    let runProgram (program: int array) noun verb =
-        let mutable memory = program
-        let rec run pc =
-            match memory.[pc] with
-            | 1 ->
-                let a = memory.[memory.[pc + 1]]
-                let b = memory.[memory.[pc + 2]]
-                let c = memory.[pc + 3]
-                memory.[c] <- a + b
-                run (pc + 4)
-            | 2 ->
-                let a = memory.[memory.[pc + 1]]
-                let b = memory.[memory.[pc + 2]]
-                let c = memory.[pc + 3]
-                memory.[c] <- a * b
-                run (pc + 4)
-            | 99 -> ()
-            | _ -> failwith "Unknown opcode"
-        memory.[1] <- noun
-        memory.[2] <- verb
-        run 0
-        memory.[0]
-
-    let findInputs targetOutput program =
+    let findPair forOutput (program : int array) = 
         [for noun in 0..99 do
             for verb in 0..99 do
-                if runProgram program noun verb = targetOutput then
-                    yield (noun, verb)]
+                if gravityAssist noun verb program = forOutput then
+                    yield (noun, verb)] |> Seq.head
 
-    let public part1 (input: string) = input.Split ',' |> Array.map int |> gravityAssist
-    let public part2 (input: string) = 
-        let (noun, verb) = input.Split ',' |> Array.map int |> findInputs 19690720 |> List.head
-        100 * noun + verb
+    let computePassword (noun, verb) = 100 * noun + verb
+    
+    let public part1 (input: string) = input.Split ',' |> Array.map int |> gravityAssist 12 2
+    let public part2 (input: string) = input.Split ',' |> Array.map int |> findPair 19690720 |> computePassword
 
 
 
 
 [<EntryPoint>]
 let main _ = 
-    let s = [1 .. 10] |> Seq.chunkBySize 4 |> Seq.map List.ofArray |> List.ofSeq
-    printfn $"{s}"
     printfn "Day 1, Part 1: %d" (day1.part1 <| inputLines 1)
     printfn "Day 1, Part 2: %d" (day1.part2 <| inputLines 1)
     printfn "Day 2, Part 1: %d" (day2.part1 <| inputText 2)
